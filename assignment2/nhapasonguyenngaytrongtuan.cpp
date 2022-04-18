7b72b7e00b8c87b6c6359ded051513736da56f5c
liazz577 <101612397+liazz577@users.noreply.github.com>
2022-04-18 13:31:44 +0700

    assignment2bai1

---
+++ assignment2/nhapasonguyenngaytrongtuan.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int maint(){
+int n;
+	printf("n");
+	scanf("%d",&n);
+	if(2 =< n =< 7){
+		printf("day la thu 2-7");
+	}
+else if(n=8){
+	printf("day la chu nhat");
+}else{
+	printf("day khong phai la ngay trong tuan")
+}
+}
+
+
